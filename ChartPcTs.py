00b24756e45711465d8bf3d16070102eea208203
364hao <129530250+364hao@users.noreply.github.com>
2024-09-22 17:08:01 +0800

    fix: level

--- ChartPcTs.py
+++ ChartPcTs.py
@@ -40,7 +40,7 @@ for id, info in track(infos.items(), description = "WritingPhichainProject..."):
             f.write(f'"name": {info["Name"]},')
             f.write(f'"level": {info["difficulty"][level]}')
             f.write("}")
-        shutil.copy(f"Phigros_Resource/Chart_{level}/{id}.json", baseDir)
+        shutil.copy(f"Phigros_Resource/Chart_{levels[level]}/{id}.json", baseDir)
         os.rename(baseDir+f'\\{id}.json',"chart.json")
         shutil.copy(f"Phigros_Resource/Illustration/{id}.png", baseDir)
         os.rename(baseDir+f'\\{id}.png',"illustration.png")

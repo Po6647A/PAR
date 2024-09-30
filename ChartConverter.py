1d404987979f6015034cbeb9ebe6ccbcbe65ede6
364hao <129530250+364hao@users.noreply.github.com>
2024-09-30 19:48:41 +0800

    fix: path err

--- ChartConverter.py
+++ ChartConverter.py
@@ -14,7 +14,7 @@ for difficultyDir in [EZ, HD, IN, AT]:
 	for chartName in os.listdir(difficultyDir):
 		print(chartName)
 		chartPath = os.path.join(difficultyDir, chartName)
-		processes.append(subprocess.Popen(converter+' --input official '+'--output rpe '+chartPath))
+		processes.append(subprocess.Popen([converter, '--input', 'official', '--output', 'rpe', chartPath]))
 for p in processes:
 	p.wait()
 processes = []
@@ -23,7 +23,7 @@ for difficultyDir in [EZ, HD, IN, AT]:
 		if chartName.endswith('.rpe.json'):
 			print(chartName)
 			chartPath = os.path.join(difficultyDir, chartName)
-			processes.append(subprocess.Popen(converter+' --input rpe'+' --output official '+chartPath))
+			processes.append(subprocess.Popen([converter, '--input', 'rpe', '--output', 'official', chartPath]))
 for p in processes:
 	p.wait()
 	

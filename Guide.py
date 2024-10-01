ad9875e1d9b15b12db34f77d0081b900b5cc6be4
364hao <129530250+364hao@users.noreply.github.com>
2024-10-01 13:30:40 +0800

    chore: test

--- Guide.py
+++ Guide.py
@@ -1,2 +1,13 @@
-with open("README.md", "w", encoding = "utf-8") as f:
-	
+import os
+
+if __name__ =='__main__':
+	if not os.path.exists('docs'):
+		os.mkdir('docs')
+	with open(os.path.join('Phigros_Resource', 'manifest.json'), 'r', encoding = 'utf-8') as f:
+		version = json.load(f)['version_name']
+	versionDir = os.path.join('docs', version)
+	if not os.path.exists(versionDir):
+		os.mkdir(versionDir)
+		
+		with open("Main.md", "w", encoding = "utf-8") as f:
+			pass
--- Upload.py
+++ Upload.py
@@ -16,24 +16,24 @@ def upload(release, filepath):
         content_type = "application/octet-stream"
     )
 if __name__ == '__main__':
-    g = Github(os.environ.get('GITHUB_TOKEN'))
+    g = Github(os.environ.get('github_token'))
     repo = g.get_repo("364hao/Test")
     resDir = os.path.abspath('Phigros_Resource')
     with open(os.path.join(resDir, 'manifest.json'), 'r', encoding = 'utf-8') as f:
         version = 'v' + json.load(f)['version_name']
-        if not exist_release(repo, version):
-            release = repo.create_git_release(
-                tag = version,
-                name = f'Version {version} full assests',
-                message = "From Apkpure, Github Action bot Generate"
-            )
-            upload(os.path.join(resDir, 'icon.png'))
-            upload(os.path.join(resDir, 'difficulty.tsv'))
-            upload(os.path.join(resDir, 'collection.tsv'))
-            upload(os.path.join(resDir, 'avatar.txt'))
-            upload(os.path.join(resDir, 'illustration.txt'))
-            upload(os.path.join(resDir, 'info.tsv'))
-            upload(os.path.join(resDir, 'music-info.json'))
-            upload(os.path.join(resDir, 'tips.txt'))
-            upload(os.path.join(resDir, 'single.txt'))
-            
+    if not exist_release(repo, version):
+        release = repo.create_git_release(
+            tag = version,
+            name = f'Version {version} full assests',
+            message = "From Apkpure, Github Action bot Generate"
+        )
+        upload(os.path.join(resDir, 'icon.png'))
+        upload(os.path.join(resDir, 'difficulty.tsv'))
+        upload(os.path.join(resDir, 'collection.tsv'))
+        upload(os.path.join(resDir, 'avatar.txt'))
+        upload(os.path.join(resDir, 'illustration.txt'))
+        upload(os.path.join(resDir, 'info.tsv'))
+        upload(os.path.join(resDir, 'music-info.json'))
+        upload(os.path.join(resDir, 'tips.txt'))
+        upload(os.path.join(resDir, 'single.txt'))
+        

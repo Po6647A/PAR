8ce4b9347091973aad334f3db94649eccae2962d
364hao <129530250+364hao@users.noreply.github.com>
2024-10-02 09:46:25 +0800

    Update UploadRelease.py

--- UploadRelease.py
+++ UploadRelease.py
@@ -18,21 +18,18 @@ if __name__ == '__main__':
     resDir = os.path.abspath('Phigros_Resource')
     with open(os.path.join(resDir, 'manifest.json'), 'r', encoding = 'utf-8') as f:
         version = json.load(f)['version_name']
-    try:
-        r = repo.create_git_release(
-            tag = 'v' + version,
-            name = f'Version {version} full assests',
-            message = "From Apkpure, Github Action bot Generate"
-        )
-        upload(r, os.path.join(resDir, 'icon.png'))
-        upload(r, os.path.join(resDir, 'difficulty.tsv'))
-        upload(r, os.path.join(resDir, 'collection.tsv'))
-        upload(r, os.path.join(resDir, 'avatar.txt'))
-        upload(r, os.path.join(resDir, 'illustration.txt'))
-        upload(r, os.path.join(resDir, 'info.tsv'))
-        upload(r, os.path.join(resDir, 'music-info.json'))
-        upload(r, os.path.join(resDir, 'tips.txt'))
-        upload(r, os.path.join(resDir, 'single.txt'))
-        main(r, upload)
-    except:
-        pass
+    r = repo.create_git_release(
+        tag = 'v' + version,
+        name = f'Version {version} full assests',
+        message = "From Apkpure, Github Action bot Generate"
+    )
+    upload(r, os.path.join(resDir, 'icon.png'))
+    upload(r, os.path.join(resDir, 'difficulty.tsv'))
+    upload(r, os.path.join(resDir, 'collection.tsv'))
+    upload(r, os.path.join(resDir, 'avatar.txt'))
+    upload(r, os.path.join(resDir, 'illustration.txt'))
+    upload(r, os.path.join(resDir, 'info.tsv'))
+    upload(r, os.path.join(resDir, 'music-info.json'))
+    upload(r, os.path.join(resDir, 'tips.txt'))
+    upload(r, os.path.join(resDir, 'single.txt'))
+    main(r, upload)

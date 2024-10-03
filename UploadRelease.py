c743fc99772f6dcb6c91b12e49bd4c5cd8b4d11b
364hao <129530250+364hao@users.noreply.github.com>
2024-10-03 09:31:47 +0800

    refactor: gh instead pygithub

--- UploadRelease.py
+++ UploadRelease.py
@@ -1,41 +1,49 @@
-from github import Github, Auth
 import json
 import os
+import subprocess
 from MarkdownGuide import main
 
-def upload(release, filepath, name = None):
+def upload(release, filepath, v, name = None):
     if os.path.exists(filepath):
         if name == None:
             name = os.path.basename(filepath)
-        try:
-            release.upload_asset(
-                path = filepath,
-                name = name,
-                content_type = "application/octet-stream"
-            )
-        except:
-            pass
+        subprocess.Popen(
+            [
+                'gh',
+                'release',
+                'upload',
+                f'v{v}',
+                f'{filepath}',
+                '--name',
+                f'{name}'
+            ]
+        )
 if __name__ == '__main__':
-    g = Github(auth = Auth.Token(os.environ.get('GITHUB_TOKEN')))
-    repo = g.get_repo("364hao/Test")
     resDir = os.path.abspath('Phigros_Resource')
     with open(os.path.join(resDir, 'manifest.json'), 'r', encoding = 'utf-8') as f:
         version = json.load(f)['version_name']
     try:
-        r = repo.create_git_release(
-            tag = 'v' + version,
-            name = f'Version {version} full assests',
-            message = "From Apkpure, Github Action bot Generate"
+        subprocess.run(
+            [
+                'gh', 
+                'release', 
+                'create',
+                f'v{version}', 
+                '--title', 
+                f'Version {version} full assests',
+                '--notes',
+                'From Apkpure, Github Action bot Generate'
+            ]
         )
-        upload(r, os.path.join(resDir, 'icon.png'))
-        upload(r, os.path.join(resDir, 'difficulty.tsv'))
-        upload(r, os.path.join(resDir, 'collection.tsv'))
-        upload(r, os.path.join(resDir, 'avatar.txt'))
-        upload(r, os.path.join(resDir, 'illustration.txt'))
-        upload(r, os.path.join(resDir, 'info.tsv'))
-        upload(r, os.path.join(resDir, 'music-info.json'))
-        upload(r, os.path.join(resDir, 'tips.txt'))
-        upload(r, os.path.join(resDir, 'single.txt'))
+        upload(r, os.path.join(resDir, 'icon.png'), version)
+        upload(r, os.path.join(resDir, 'difficulty.tsv'), version)
+        upload(r, os.path.join(resDir, 'collection.tsv'), version)
+        upload(r, os.path.join(resDir, 'avatar.txt'), version)
+        upload(r, os.path.join(resDir, 'illustration.txt'), version)
+        upload(r, os.path.join(resDir, 'info.tsv'), version)
+        upload(r, os.path.join(resDir, 'music-info.json'), version)
+        upload(r, os.path.join(resDir, 'tips.txt'), version)
+        upload(r, os.path.join(resDir, 'single.txt'), version)
         main(upload, r)
     except:
         pass
